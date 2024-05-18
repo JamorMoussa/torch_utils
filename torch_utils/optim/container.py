bfd0e15f5cb7982d7ca0df3b03b53234f2d8c715
JamorMoussa <moussajamorsup@gmail.com>
2024-05-18 21:21:16 +0100

    add step, zero_grad to OptimizerContainer (#1)

--- torch_utils/optim/container.py
+++ torch_utils/optim/container.py
@@ -3,9 +3,16 @@ import torch.optim as optim
 
 __all__ = ["OptimizerContainer", ]
 
+
 class OptimizerContainer:
 
     opt: optim.Optimizer
 
     def __init__(self, opt: optim.Optimizer) -> None:
         self.opt = opt
+
+    def zero_grad(self) -> None:
+        self.opt.zero_grad()
+
+    def step(self):
+        self.opt.step()
